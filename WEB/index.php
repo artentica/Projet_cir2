59c4143dd41a54388b81ec4cd20966ac89f4ed2f
root <root@ebianWheezy7.5>
2014-05-16 14:31:45 +0200

    ajout dossier web

---
+++ WEB/index.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+		Coucou les iseniens
+		<br/>
+
+		<a target="_BLANK" href="phpmyadmin">PHPMYADMIN</a><br/>
+		<a target="_BLANK" href="Projet_cir2">PROJET</a><br/>
+		<a target="_BLANK" href="http://github.com/artentica/Projet_cir2">GITHUB</a><br/>
+
+</body>
+</html>
