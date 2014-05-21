0bf7ae081b7ad009f3ea6c7853c240fc4b5c63b0
artentica <riouallonvincent@gmail.com>
2014-05-21 12:24:51 +0200

    connexion.php

--- WEB/connexion.php
+++ WEB/connexion.php
@@ -1,44 +1,73 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php 
-        	require 'include/head.php';
-        ?>
-       
-    </head>
+<head>
+	<?php 
+	require 'include/head.php';
+	require 'include/bdd.php';
 
-    <body>
-    <form class="form-horizontal"
-><fieldset>
 
-<!-- Form Name -->
-<legend>Entrez votre identifiant et votre mot de passe</legend>
 
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Identifiant:</label>  
-  <div class="col-md-4">
-  <input id="textinput" name="textinput" class="form-control input-md" type="text">  
-  </div>
-</div>
+	if(isset($_POST['username']) && isset($_POST['password'])){
 
-<!-- Password input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="passwordinput">Mot de passe:</label>
-  <div class="col-md-4">
-    <input id="passwordinput" name="passwordinput" class="form-control input-md" type="password">
-  </div>
-</div>
+		$rep=Select('SELECT username, password FROM utilisateur');
+		while($data = mysql_fetch_assoc($rep)){
+           if ($data['LOGIN']==$_POST['username'] && $data['PASSWORD']==$_POST['password']) {
+             $_SESSION['log'] = 'ok';
+             header('Location: choose.php');
+           }
+	}
+}
+	?>
 
-<!-- Button -->
-<div class="form-group">
-  <div class="col-md-4">
-    <button id="singlebutton" name="singlebutton" class="btn btn-primary">SE CONNECTER</button>
-  </div>
-</div>
+</head>
 
-</fieldset>
-</form>
-    
-    </body>
+<body>
+	<div class="container" >
+		<div class="row">
+
+
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<h3 class="panel-title">Entrez votre identifiant et votre mot de passe</h3>
+				</div>
+				<div class="panel-body">
+					<div class="row">
+						<form class="form-horizontal" action="connexion.php" method="POST">
+
+
+							<!-- Text input-->
+							<div class="form-group">
+								<label class="col-md-4 control-label" for="textinput">Identifiant:</label>  
+								<div class="col-md-4">
+									<input id="textinput" name="username" class="form-control input-md input-xs" type="text">  
+								</div>
+							</div>
+
+							<!-- Password input-->
+							<div class="form-group">
+								<label class="col-md-4 control-label" for="passwordinput">Mot de passe:</label>
+								<div class="col-md-4">
+									<input id="passwordinput" name="password" class="form-control input-md" type="password">
+								</div>
+							</div>
+
+							<!-- Button -->
+							<div class="form-group">
+								<div class="col-md-4 col-md-offset-4">
+									<button id="singlebutton" name="singlebutton" class="btn btn-primary">SE CONNECTER</button>
+								</div>
+							</div>
+
+
+						</form>
+					</div>
+				</div>
+
+
+
+			</div>
+		</div>
+	</div>
+</body>
 </html>
