6d220c9eccbc570ccbfbc2883d891b1c64a162d8
miton18 <rcd18@hotmail.fr>
2014-05-21 12:23:42 +0200

    correction bdd.php

--- WEB/choose.php
+++ WEB/choose.php
@@ -8,11 +8,21 @@
     </head>
     <body>
     	<div class="container">
-    		<div class="navbar navbar-inverse">
+    		<nav class="navbar navbar-inverse">
     			<ul class="nav navbar-nav">
     				<li>	<a href="#" >Créer un projet</a>	</li>
-    				<li>	<?php deco(); ?>	</li>
+    				<li class="pull-right">	<?php deco(); ?>	</li>
     			</ul>
+    		</nav>
+    		<div class="row" >
+    			<div class="panel panel-primary">
+				  <div class="panel-heading">
+				    <h3 class="panel-title text-center">CHOIX DU PROJET</h3>
+				  </div>
+				  <div class="panel-body">
+				    Panel content
+				  </div>
+				</div>
     		</div>
     	</div>
     </body>
