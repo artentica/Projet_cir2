de36c74df394ced5389b66aa1b391975a6cef776
artentica <riouallonvincent@gmail.com>
2014-06-03 11:53:35 +0200

    runner

--- JAVA/TestRunner/Runner.java
+++ JAVA/TestRunner/Runner.java
@@ -5,6 +5,7 @@ import org.junit.runner.*;
 public class Runner{
 public static void main (String [] args)
 	{
-		JUnitCore.runClasses(MoneyTest.class);
+
+		Result result = JUnitCore.runClasses(MoneyTest.class);
     }
 }
--- JAVA/TestRunner/make.sh
+++ JAVA/TestRunner/make.sh
@@ -4,5 +4,5 @@ javac /var/www/Projet_cir2/JAVA/TestRunner/projet/depot/Money.java
 #compile la classe de test
 javac -cp /usr/share/java/junit4.jar:/var/www/Projet_cir2/JAVA/TestRunner/projet/depot:. /var/www/Projet_cir2/JAVA/TestRunner/projet/test/MoneyTest.java
 
-javac -cp /usr/share/java/junit4.jar:. /var/www/Projet_cir2/JAVA/TestRunner/Runner.java
-java -cp /usr/share/java/junit4.jar:/var/www/Projet_cir2/JAVA/TestRunner/projet/test:. /var/www/Projet_cir2/JAVA/TestRunner/Runner
+javac -cp /usr/share/java/junit4.jar:/var/www/Projet_cir2/JAVA/TestRunner/projet/test:. /var/www/Projet_cir2/JAVA/TestRunner/Runner.java
+java -cp /usr/share/java/junit4.jar:/var/www/Projet_cir2/JAVA/TestRunner/projet/test:/var/www/Projet_cir2/JAVA/TestRunner/projet/depot:. Runner
