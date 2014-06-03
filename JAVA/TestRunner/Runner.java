d7e4df64a04d88ae32dc3200816a83bc1b9887cf
artentica <riouallonvincent@gmail.com>
2014-06-03 10:41:03 +0200

    make.sh

---
+++ JAVA/TestRunner/Runner.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import org.junit.Assert.*;
+import org.junit.runner.*;
+
+public class Runner{
+public static void main (String [] args)
+	{
+		JUnitCore.runClasses(MoneyTest.class);
+    }
+}
