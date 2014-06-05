0aa4e16697a880fb140d2c5751b83e94347dc9da
root <root@ebianWheezy7.5>
2014-06-05 16:16:13 +0200

    ...........

--- WEB/include/bdd.php
+++ WEB/include/bdd.php
@@ -47,31 +47,38 @@
 
 	//AJOUT DE TESTS
 	function addTest( $id, $no, $nom, $note ){
-		$req = Select( 'SELECT TEST_NUM FROM TEST WHERE NAME=' . $nom);
-		//print_r($req);
+		$req = Select( "SELECT TEST_NUM FROM TEST WHERE NAME='$nom'");
+		//print_r($req);		//DEBUG
 		if ( !empty($req[0]) ){
 			return $req[0][0];
 		}
 		else //ON AJOUTE LE TEST
 		{
+			$GLOBALS['num_test'] ++ ;
 			return Ins("INSERT INTO TEST VALUES( $id, $no, '$nom', $note )");
 		}
 	}
 
 	//AJOUT DU SOUS TEST
 	function addSSTest( $id, $no, $noo, $typ, $val){
-		$req = Select('SELECT SUBTEST_NUM FROM SUB_TEST WHERE VALEUR=' . $val);
-		if ( $req[0] )
+		$req = Select("SELECT SUBTEST_NUM FROM SUB_TEST WHERE VALEUR='$val'");
+		print_r( $req );
+		if ( !empty($req[0]) )
 		{
-			return $req[0][0];
+			return $req[0]["TEST_NUM"];
 		}
 		else{
-			return Ins("INSERT INTO SUB_TEST VALUES( $id, $no, $noo, '$typ', '$val' )");
+			$sql ="INSERT INTO SUBTEST VALUES( $id, $no, $noo, '$typ', '$val' )";
+			echo $sql;
+			$GLOBALS['num_ss_test'] ++ ;
+			return Ins($sql);
 		}
 	}
 
 	//AJOUT DU RESULTAT
 	function addResult( $user , $P, $no, $noo, $s, $d ){
-		Ins("INSERT INTO RESULT VALUES('$user', $P, $no, $noo, $s, '$d')");
+		$sql = "INSERT INTO RESULT VALUES('$user', $P, $no, $noo, $s, '$d')";
+		echo($sql);
+		Ins($sql);
 	}
 ?>
