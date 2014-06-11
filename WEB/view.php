cac84e58970e4e255bf364aea29448013ebbf66e
miton18 <rcd18@hotmail.fr>
2014-06-11 20:27:40 +0200

    debug view

--- WEB/view.php
+++ WEB/view.php
@@ -57,111 +57,110 @@
                 <?php
 
                   $P = $_GET['P'];
-                  $use = $_GET['U'];
+                  $user = $_GET['U'];
 
-            if( C_prof() )  //SI prof
-            {
-                  $sql = "SELECT T.NAME, T.MARK, S.KIND, S.VALEUR, R.STATUS, R.DESCRIPTION 
-                          FROM TEST T
+                  if( C_prof() )  //SI prof
+                  {
+                    $sql = "SELECT T.NAME, T.MARK, S.KIND, S.VALEUR, R.STATUS, R.DESCRIPTION 
+                            FROM TEST T
 
-                          INNER JOIN SUBTEST S
-                          ON S.PROJECT_ID = T.PROJECT_ID
-                            AND S.TEST_NUM = T.TEST_NUM
+                            INNER JOIN SUBTEST S
+                            ON S.PROJECT_ID = T.PROJECT_ID
+                              AND S.TEST_NUM = T.TEST_NUM
 
-                          INNERJOIN RESULT R
-                          ON R.PROJECT_ID = T.PROJECT_ID
-                            AND R.TEST_NUM = T.TEST_NUM
-                            AND R.SUBTEST_NUM = S.SUBTEST_NUM
+                            INNER JOIN RESULT R
+                            ON R.PROJECT_ID = T.PROJECT_ID
+                              AND R.TEST_NUM = T.TEST_NUM
+                              AND R.SUBTEST_NUM = S.SUBTEST_NUM
 
-                          WHERE R.PROJECT_ID=$P 
-                            AND R.LOGIN='$use'";
-                  
-                  $rep      = Select( $sql );
-                  $note     = 0;
-                  $_SESSION['bareme_'. $P ] =   array();
-                  $_SESSION['bareme']   =   0;
+                            WHERE R.PROJECT_ID=$P 
+                              AND R.LOGIN='$user'";
+                    
+                    $rep      = Select( $sql );
+                    print_r($rep);
+                    $note     = 0;
 
-                  foreach ($rep as $key => $val) {
-                    $O = ( $val[4] > 0 ) ? 'OK' : 'KO';
-                    if ($val[4]>0) {
-                      $points=round($val[1] / $val[2],2);
-                    }
-                    else $points=0;
-                    echo "<tr>
-                            <td> $val[0] </td>
-                            <td> $points </td>
-                            <td> $val[2] </td>
-                            <td> $val[3] </td>
-                            <td>    $O   </td>
-                            <td> $val[5] </td>
-                          </tr>" ;
-                    $note     += $points;
+                    foreach ($rep as $key => $val) {
+                      $OK = ( $val[4] > 0 ) ? 'OK' : 'KO'; //SI STATUS >0
 
+                      if ($val[4]>0)                        //SI STATUS >0
+                        $points=round($val[1] / $val[2],2); //  NOTE max / NB fois appel de la foncfion
+                      else 
+                        $points=0;
 
-                  }
-                  $B    = Select('SELECT SUM(MARK) FROM TEST WHERE PROJECT_ID=' . $P );
-                  $bareme = $B[0][0];
-                  if($note >$bareme)$note=$bareme;
-            }
-            else{ // SI ELEVE
-                  $sql = "SELECT T.NAME, R.STATUS
-                          FROM TEST T
-
-                          INNER JOIN SUBTEST S
-                          ON S.PROJECT_ID = T.PROJECT_ID
-                            AND S.TEST_NUM = T.TEST_NUM
-
-                          INNER JOIN RESULT R
-                          ON R.PROJECT_ID = T.PROJECT_ID
-                            AND R.TEST_NUM = T.TEST_NUM
-                            AND R.SUBTEST_NUM = S.SUBTEST_NUM
-
-                          WHERE R.PROJECT_ID=$P 
-                            AND R.LOGIN='$use'";
-                  
-                  $rep = Select( $sql );
-                  $name_tmp="lol";
-                  $name_tmp2="loli";
-                  $total=0;
-                  $total_tmp=0;
-                  $total_tmp2=0;
-                  foreach ($rep as $key => $val) {
-                    //$O = ( $val[4] > 0 ) ? 'OK' : 'KO';
-                    $name_fct=$val[0];
-
-                    if ($name_tmp2==$name_tmp && $name_tmp2!="loli") {
-                      $total++;
-                    }
-                    elseif ($name_tmp2!="lol" && $name_tmp2!="loli"){
-                      $total++;
-                    echo "<tr>
-                            <td> $name_tmp2 </td>
-                            <td> ".$total."</td>
-                            <td>   </td>
-                          </tr>" ;
-                   
-                    $total=0;
-                        }
-                         $name_tmp2=$name_tmp;
-                    $name_tmp=$val[0];
-                  }
-                  if ($name_tmp!=$name_tmp2) {
-                   echo "<tr>
-                            <td> $name_tmp2 </td>
-                            <td> ".++$total."</td>
-                            <td>   </td>
-                          </tr>" ;
-                  }
-                  else $total+=2;
                       echo "<tr>
-                            <td> $name_tmp </td>
-                            <td> ".$total."</td>
-                            <td>   </td>
-                          </tr>" ;
+                              <td> $val[0] </td>
+                              <td> $points </td>
+                              <td> $val[2] </td>
+                              <td> $val[3] </td>
+                              <td>    $OK   </td>
+                              <td> $val[5] </td>
+                            </tr>" ;
+                      $note     += $points;
+
+
+                    }
+                    $B    = Select('SELECT SUM(MARK) FROM TEST WHERE PROJECT_ID=' . $P );
+                    $bareme = $B[0][0];
+                    if($note >$bareme)$note=$bareme;
+                  }
+                  else{ // SI ELEVE
+                    $rep = Select("SELECT T.NAME, R.STATUS
+                            FROM TEST T
+
+                            INNER JOIN SUBTEST S
+                            ON S.PROJECT_ID = T.PROJECT_ID
+                              AND S.TEST_NUM = T.TEST_NUM
+
+                            INNER JOIN RESULT R
+                            ON R.PROJECT_ID = T.PROJECT_ID
+                              AND R.TEST_NUM = T.TEST_NUM
+                              AND R.SUBTEST_NUM = S.SUBTEST_NUM
+
+                            WHERE R.PROJECT_ID=$P 
+                              AND R.LOGIN='$use'");
+                    $name_tmp="lol";
+                    $name_tmp2="loli";
+                    $total=0;
+                    $total_tmp=0;
+                    $total_tmp2=0;
+                    foreach ($rep as $key => $val) {
+                      //$O = ( $val[4] > 0 ) ? 'OK' : 'KO';
+                      $name_fct=$val[0];
+
+                      if ($name_tmp2==$name_tmp && $name_tmp2!="loli") {
+                        $total++;
+                      }
+                      elseif ($name_tmp2!="lol" && $name_tmp2!="loli"){
+                        $total++;
+                      echo "<tr>
+                              <td> $name_tmp2 </td>
+                              <td> ".$total."</td>
+                              <td>   </td>
+                            </tr>" ;
+                     
+                      $total=0;
+                          }
+                           $name_tmp2=$name_tmp;
+                      $name_tmp=$val[0];
+                    }
+                    if ($name_tmp!=$name_tmp2) {
+                     echo "<tr>
+                              <td> $name_tmp2 </td>
+                              <td> ".++$total."</td>
+                              <td>   </td>
+                            </tr>" ;
+                    }
+                    else $total+=2;
+                        echo "<tr>
+                              <td> $name_tmp </td>
+                              <td> ".$total."</td>
+                              <td>   </td>
+                            </tr>" ;
                     
                     
 
-            }
+                  }
           ?>
               </table>
             </div>
